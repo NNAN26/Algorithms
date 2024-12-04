46cc27f56c24fa421b36d309f47e7873ddb43945
kraianan Rattana <wipu0811@gmail.com>
2024-12-04 11:49:02 +0700

    ส่งงาน

--- factorial.java
+++ factorial.java
@@ -1,14 +1,18 @@
 import java.util.Scanner;
 
 public class factorial {
-    Scanner scanIn = new Scanner(System.in);
-    int a = scanIn.nextInt();
-    //scanIn.close();
-    
-    int fact = 1;
-    for(int i=1; i<=a ;i++){
-        fact = fact*i;
-    }
-    System.out.println(fact);
 
+    public static void main(String[] args) {
+        Scanner scanIn = new Scanner(System.in);
+        System.out.println("Enter a number ");
+        int a = scanIn.nextInt();
+        scanIn.close();
+
+        int fact = 1;
+        for (int i = 1; i <= a; i++) {
+            System.out.println(i + "! =" + fact);
+            fact += fact * i;
+
+        }
+    }
 }
