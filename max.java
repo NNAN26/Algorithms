e9ec8779523ee5c08b422071e89255eecadb9189
kraianan Rattana <wipu0811@gmail.com>
2024-12-04 10:56:19 +0700

    ส่งงาน

---
+++ max.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class max {
+    
+public static void main(String[] args) {
+    Scanner scanIn = new Scanner (System.in);
+    int a = scanIn.nextInt();
+    int b = scanIn.nextInt();
+    int c = scanIn.nextInt();
+    scanIn.close();
+
+    if(a>b && a>c){
+        System.out.println("A");
+    }else if(b>a && b>c){
+        System.out.println("B");
+    }else if(c>a && c>b){
+        System.out.println("C");
+    }
+
+    System.out.println();
+
+}
+}
